ac8e840a01a5e10520525f52bf6622a1443a35b7
aleja9992 <aleja99908@gmail.com>
2026-08-31 23:30:41 -0500

    feat: Update firebase configuration to use environment variables

--- listacontactos/firebase.js
+++ listacontactos/firebase.js
@@ -1,16 +1,14 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyARsjW3ScztN3MKcD0LasRCfdnY6Gy-PfQ",
-  authDomain: "listacontactos-4b1ef.firebaseapp.com",
-  projectId: "listacontactos-4b1ef",
-  storageBucket: "listacontactos-4b1ef.firebasestorage.app",
-  messagingSenderId: "362059964161",
-  appId: "1:362059964161:web:8acb185daf78991a05c500"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
 export const db = getFirestore(app);
